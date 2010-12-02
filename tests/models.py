1a379522c70f82da92d0bfbed3772862fa002730
aruseni <aruseni.magiku@gmail.com>
2010-12-02 05:54:46 +0300

    Add models Test, Question and Answer

--- tests/models.py
+++ tests/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Test(models.Model):
+    title = models.CharField(max_length=255)
+
+    def __unicode__(self):
+        return self.title
+
+class Question(models.Model):
+    title = models.CharField(max_length=255)
+    test = models.ForeignKey(Test)
+
+    def __unicode__(self):
+        return self.title
+
+class Answer(models.Model):
+    title = models.CharField(max_length=255)
+    question = models.ForeignKey(Question)
+    is_true = models.BooleanField()
+
+    def __unicode__(self):
+        return self.title
