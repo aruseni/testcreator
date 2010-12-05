c73afcb335b295c5a630ce2597caf5c4fdba66bc
aruseni <aruseni.magiku@gmail.com>
2010-12-05 03:26:39 +0300

    Add urlpattern for serving static files

--- urls.py
+++ urls.py
@@ -1,5 +1,10 @@
 from django.conf.urls.defaults import *
 
+import os.path
+import sys
+
+PROJECT_ROOT = os.path.dirname(__file__)
+
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
@@ -7,6 +12,9 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Example:
     # (r'^testcreator/', include('testcreator.foo.urls')),
+
+    (r'^static/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': os.path.join(PROJECT_ROOT, 'media')}),
 
     # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
     # to INSTALLED_APPS to enable admin documentation:
