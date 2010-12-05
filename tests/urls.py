d4c3f2f019db936216d429878944f03df7757992
aruseni <aruseni.magiku@gmail.com>
2010-12-05 04:12:24 +0300

    Add test_detail view, template and urlpattern

--- tests/urls.py
+++ tests/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('testcreator.tests.views',
     (r'^$', 'index'),
+    (r'^test_(?P<test_id>\d+)/$', 'test_detail'),
 )
--- tests/views.py
+++ tests/views.py
@@ -1,9 +1,14 @@
 # Create your views here.
 
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, get_object_or_404
 
 from testcreator.tests.models import Test
 
 def index(request):
     tests = Test.objects.all()
     return render_to_response('tests/index.html', {'test_list': tests,})
+
+def test_detail(request, test_id):
+    test = get_object_or_404(Test, id=test_id)
+    questions = test.question_set.all()
+    return render_to_response('tests/test_detail.html', {'test': test, 'question_list': questions,})
