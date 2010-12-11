ff92a9f3578ce59048dd52ddba53f6cf99a53409
aruseni <aruseni.magiku@gmail.com>
2010-12-11 11:48:36 +0300

    Add edit_question method, template and urlpattern

--- tests/urls.py
+++ tests/urls.py
@@ -4,4 +4,5 @@ urlpatterns = patterns('testcreator.tests.views',
     (r'^$', 'index'),
     (r'^test_(?P<test_id>\d+)/$', 'test_detail'),
     (r'^question_(?P<question_id>\d+)/$', 'question_detail'),
+    (r'^edit_question_(?P<question_id>\d+)/$', 'edit_question'),
 )
--- tests/views.py
+++ tests/views.py
@@ -1,8 +1,11 @@
 # Create your views here.
 
 from django.shortcuts import render_to_response, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.template import RequestContext
 
-from testcreator.tests.models import Test, Question
+from testcreator.tests.models import Test, Question, Answer
 
 def index(request):
     tests = Test.objects.all()
@@ -16,4 +19,19 @@ def test_detail(request, test_id):
 def question_detail(request, question_id):
     question = get_object_or_404(Question, id=question_id)
     answers = question.answer_set.all()
-    return render_to_response('tests/question_detail.html', {'question': question, 'answer_list': answers,})
+    return render_to_response('tests/question_detail.html', {'question': question, 'answer_list': answers,},
+    context_instance=RequestContext(request))
+
+def edit_question(request, question_id):
+    question = get_object_or_404(Question, id=question_id)
+    question.answer_set.all().delete()
+    for k, v in request.POST.items(): # for every key and value
+        if ("answer" in k or "new" in k) and (not "true" in k):
+            answer = Answer()
+            answer.title = v
+            answer.question = question
+            is_true = request.POST.get(k+"_true", "");
+            if is_true:
+                answer.is_true = True
+            answer.save()
+    return HttpResponseRedirect(reverse('testcreator.tests.views.question_detail', args=(question.id,)))
