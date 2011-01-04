fc305d5d4256a1f80a2b4c28a4bd0626253ed5f2
aruseni <aruseni.magiku@gmail.com>
2011-01-04 22:55:12 +0300

    Edit delete_answer urlpattern

--- tests/urls.py
+++ tests/urls.py
@@ -6,6 +6,6 @@ urlpatterns = patterns('testcreator.tests.views',
     (r'^question_(?P<question_id>\d+)/$', 'question_detail'),
     (r'^edit_question_(?P<question_id>\d+)/$', 'edit_question'),
     (r'^delete_question_(?P<question_id>\d+)/$', 'delete_question'),
-    (r'^answer_question_(?P<answer_id>\d+)/$', 'delete_answer'),
+    (r'^delete_answer_(?P<answer_id>\d+)/$', 'delete_answer'),
     (r'^add_answer_for_question_(?P<question_id>\d+)/$', 'add_answer'),
 )
